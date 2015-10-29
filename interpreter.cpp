ebf6f9750476b73511bca4c1736f167eb28e71b5
Kimiyuki Onaka <kimiyuki95@gmail.com>
2015-10-30 01:14:18 +0900

    initial commit

---
+++ interpreter.cpp
@@ -0,0 +1,49 @@
+#include <cstdint>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <stack>
+#include <vector>
+using namespace std;
+int main(int argc, char **argv) {
+    if (argc != 2) return 1;
+    string code;
+    map<int,int> match; {
+        ifstream ifs(argv[1]);
+        if (not ifs) return 1;
+        stack<int> stk;
+        char c;
+        while (ifs >> c) {
+            if (c == '[') stk.push(code.size());
+            if (c == ']') {
+                if (stk.empty()) return 1;
+                match[stk.top()] = code.size();
+                match[code.size()] = stk.top();
+                stk.pop();
+            }
+            if (c == '+' or c == ',' or c == '-' or c == '.' or
+                c == '<' or c == '>' or c == '[' or c == ']') {
+                code += c;
+            }
+        }
+        if (not stk.empty()) return 1;
+    }
+    vector<uint8_t> mem(300000);
+    int pptr = 0;
+    int mptr = 0;
+    while (pptr < code.size()) {
+        switch (code[pptr]) {
+            case '+': ++ mem[mptr]; break;
+            case '-': -- mem[mptr]; break;
+            case '>': ++ mptr; break;
+            case '<': -- mptr; break;
+            case ',': mem[mptr] = cin.get(); if (not cin) mem[mptr] = -1; break;
+            case '.': cout << mem[mptr]; cout.flush(); break;
+            case '[': pptr = match[pptr] - 1; break;
+            case ']': if (mem[mptr]) pptr = match[pptr]; break;
+        }
+        if (mptr < 0 or mem.size() <= mptr) return 1;
+        pptr += 1;
+    }
+    return 0;
+}
